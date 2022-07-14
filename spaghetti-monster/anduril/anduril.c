b8f56d2f1e96c25ff873c2dd9f5a0464b1a81819
Mark Kuo <starryalley@gmail.com>
2022-07-14 14:38:10 +1000

    turn off all other LEDs when EV_voltage_low

--- spaghetti-monster/anduril/anduril.c
+++ spaghetti-monster/anduril/anduril.c
@@ -352,7 +352,16 @@ void low_voltage() {
     // "current_state" is volatile, so cache it to reduce code size
     StatePtr state = current_state;
 
-    // TODO: turn off aux LED(s) when power is really low
+    // turn off all other LED(s) when power is really low
+    #ifdef USE_INDICATOR_LED
+    indicator_led(0);
+    #endif
+    #ifdef USE_BUTTON_LED
+    button_led_set(0);
+    #endif
+    #ifdef USE_AUX_RGB_LEDS
+    rgb_led_set(0);
+    #endif
 
     if (0) {}  // placeholder
 
